e7593467d6fce4d789c99a08c5972417fc03c9b9
Brett Walker <brett@digitalmoksha.com>
2013-10-23 16:46:39 +0200

    allow a theme to be specified on the tag helpers

--- lib/themes_for_rails/action_view.rb
+++ lib/themes_for_rails/action_view.rb
@@ -21,31 +21,35 @@ module ThemesForRails
       theme_image_path(asset)
     end
 
-    def theme_stylesheet_path(asset, new_theme_name = self.theme_name)
-      base_theme_stylesheet_path(:theme => new_theme_name, :asset => digest_for_stylesheet(check_add_extension(asset, '.css'), new_theme_name))
+    def theme_stylesheet_path(asset, options = self.theme_name)
+      theme = options.is_a?(String) ? options : options[:theme]
+      base_theme_stylesheet_path(:theme => theme, :asset => digest_for_stylesheet(check_add_extension(asset, '.css'), new_theme_name))
     end
 
-    def theme_javascript_path(asset, new_theme_name = self.theme_name)
-      base_theme_javascript_path(:theme => new_theme_name, :asset => digest_for_javascript(check_add_extension(asset, '.js'), new_theme_name))
+    def theme_javascript_path(asset, options = self.theme_name)
+      theme = options.is_a?(String) ? options : options[:theme]
+      base_theme_javascript_path(:theme => theme, :asset => digest_for_javascript(check_add_extension(asset, '.js'), new_theme_name))
     end
 
-    def theme_image_path(asset, new_theme_name = self.theme_name)
-      base_theme_image_path(:theme => new_theme_name, :asset => digest_for_image("#{asset}", new_theme_name))
+    def theme_image_path(asset, options = self.theme_name)
+      theme = options.is_a?(String) ? options : options[:theme]
+      base_theme_image_path(:theme => theme, :asset => digest_for_image("#{asset}", new_theme_name))
     end
 
     def theme_image_tag(source, options = {})
-      image_tag(theme_image_path(source), options)
+      image_tag(theme_image_path(source, options[:theme] || self.theme_name), options.except(:theme))
     end
 
     def theme_image_submit_tag(source, options = {})
-      image_submit_tag(theme_image_path(source), options)
+      image_submit_tag(theme_image_path(source, options[:theme] || self.theme_name), options.except(:theme))
     end
 
     def theme_javascript_include_tag(*files)
       options = files.extract_options!
       options.merge!({ :type => "text/javascript" })
-      files_with_options = files.collect {|file| theme_javascript_path(file) }
-      files_with_options += [options]
+      theme = options[:theme] || self.theme_name
+      files_with_options = files.collect {|file| theme_javascript_path(file, theme) }
+      files_with_options += [options.except(:theme)]
 
       javascript_include_tag(*files_with_options)
     end
@@ -53,8 +57,9 @@ module ThemesForRails
     def theme_stylesheet_link_tag(*files)
       options = files.extract_options!
       options.merge!({ :type => "text/css" })
-      files_with_options = files.collect {|file| theme_stylesheet_path(file) }
-      files_with_options += [options]
+      theme = options[:theme] || self.theme_name
+      files_with_options = files.collect {|file| theme_stylesheet_path(file, theme) }
+      files_with_options += [options.except(:theme)]
 
       stylesheet_link_tag(*files_with_options)
     end
--- lib/themes_for_rails/digested_action_view.rb
+++ lib/themes_for_rails/digested_action_view.rb
@@ -21,31 +21,35 @@ module ThemesForRails
       digest_for_image(asset, self.theme_name)
     end
 
-    def theme_stylesheet_path(asset, new_theme_name = self.theme_name)
-      digest_for_stylesheet(check_add_extension(asset, '.css'), new_theme_name)
+    def theme_stylesheet_path(asset, options = self.theme_name)
+      theme = options.is_a?(String) ? options : options[:theme]
+      digest_for_stylesheet(check_add_extension(asset, '.css'), theme)
     end
 
-    def theme_javascript_path(asset, new_theme_name = self.theme_name)
-      digest_for_javascript(check_add_extension(asset, '.js'), new_theme_name)
+    def theme_javascript_path(asset, options = self.theme_name)
+      theme = options.is_a?(String) ? options : options[:theme]
+      digest_for_javascript(check_add_extension(asset, '.js'), theme)
     end
 
-    def theme_image_path(asset, new_theme_name = self.theme_name)
-      path_to_image(digest_for_image(asset, new_theme_name))
+    def theme_image_path(asset, options = self.theme_name)
+      theme = options.is_a?(String) ? options : options[:theme]
+      path_to_image(digest_for_image(asset, theme))
     end
 
     def theme_image_tag(source, options = {})
-      image_tag(theme_image_path(source, self.theme_name), options)
+      image_tag(theme_image_path(source, options[:theme] || self.theme_name), options.except(:theme))
     end
 
     def theme_image_submit_tag(source, options = {})
-      image_submit_tag(theme_image_path(source, self.theme_name), options)
+      image_submit_tag(theme_image_path(source, options[:theme] || self.theme_name), options.except(:theme))
     end
 
     def theme_javascript_include_tag(*files)
       options = files.extract_options!
       options.merge!({ :type => "text/javascript" })
-      files_with_options = files.collect {|file| theme_javascript_path(file) }
-      files_with_options += [options]
+      theme = options[:theme] || self.theme_name
+      files_with_options = files.collect {|file| theme_javascript_path(file, theme) }
+      files_with_options += [options.except(:theme)]
 
       javascript_include_tag(*files_with_options)
     end
@@ -53,8 +57,9 @@ module ThemesForRails
     def theme_stylesheet_link_tag(*files)
       options = files.extract_options!
       options.merge!({ :type => "text/css" })
-      files_with_options = files.collect {|file| theme_stylesheet_path(file) }
-      files_with_options += [options]
+      theme = options[:theme] || self.theme_name
+      files_with_options = files.collect {|file| theme_stylesheet_path(file, theme) }
+      files_with_options += [options.except(:theme)]
 
       stylesheet_link_tag(*files_with_options)
     end
