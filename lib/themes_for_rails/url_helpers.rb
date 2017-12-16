a5776dfc8a98342509863ada3696a60a3b87bcc9
digitalMoksha <brett@digitalmoksha.com>
2017-12-16 17:47:04 +0100

    wrap helper_method calls in respond_to?(:helper_method)

--- lib/themes_for_rails/url_helpers.rb
+++ lib/themes_for_rails/url_helpers.rb
@@ -5,9 +5,11 @@ module ThemesForRails
     extend ActiveSupport::Concern
 
     included do
-      helper_method :current_theme_stylesheet_path,
-        :current_theme_javascript_path,
-        :current_theme_image_path
+      if respond_to?(:helper_method)
+        helper_method :current_theme_stylesheet_path,
+          :current_theme_javascript_path,
+          :current_theme_image_path
+      end
     end
 
     def current_theme_stylesheet_path(asset)
